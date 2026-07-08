ebd33772806cbc5a446f09a2317ef2afab35d035
Dhiliban-BCA <ddhiliban19@gmail.com>
2026-07-08 09:49:36 +0530

    Update oddEven.js

--- oddEven.js
+++ oddEven.js
@@ -1,13 +1,10 @@
-// Write an arrow function to check whether a number is Odd or Even.
-// Return "Even" if the number is even.
-// Return "Odd" if the number is odd.
-
-// Example:
-// checkOddEven(8) ? "Even"
-// checkOddEven(5) ? "Odd"
-
 const checkOddEven = (num) => {
-    // Write your code here
+  
+if (num %2 === 0){
+    console.log('even');
+}else{
+    console.log('odd');
+}
 
 };
 
